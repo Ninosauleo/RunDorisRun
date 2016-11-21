8fbf1ebd6ea87ad31a89a9f7cba3152697b54b5d
hawtkittie <kit@hotmail.se>
2016-11-21 10:15:52 +0100

    Game Project
    Done with @Ninosauleo and @AseelNaji this project includes a
    non-official player character.

---
+++ Game/Run Doris Run!/Assets/LoadGame.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadGame : MonoBehaviour
+{
+
+    public void LoadStage()
+    {
+        Application.LoadLevel("Level 01");
+    }
+}
