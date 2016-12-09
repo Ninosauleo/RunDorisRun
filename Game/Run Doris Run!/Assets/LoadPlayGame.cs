2d0dc4df1b5127db71609aaf179f94e79690b0e3
AseelNaji <gusnajias@student.gu.se>
2016-12-09 13:48:13 +0100

    changes

---
+++ Game/Run Doris Run!/Assets/LoadPlayGame.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadPlayGame : MonoBehaviour {
+
+    public void LoadStage()
+    {
+        Application.LoadLevel("Level 01");
+    }
+}
---
+++ Game/Run Doris Run!/Assets/Scripts/Managers/LoadMainMenu.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LoadMainMenu : MonoBehaviour {
+
+	public void LoadMain ()
+	{
+		// Reload the level that is currently loaded.
+		SceneManager.LoadScene ("Game Options");
+
+}
+}
