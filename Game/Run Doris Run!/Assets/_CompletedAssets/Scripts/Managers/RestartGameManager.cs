50bde6548b6502766e3a564b6ad7f3c900c956cc
AseelNaji <gusnajias@student.gu.se>
2016-12-10 14:15:27 +0100

    Button Is working with @Ninosauleo
    restart button implemented

--- Game/Run Doris Run!/Assets/_CompletedAssets/Scripts/Managers/RestartGameManager.cs
+++ Game/Run Doris Run!/Assets/_CompletedAssets/Scripts/Managers/RestartGameManager.cs
@@ -1,13 +1,26 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class RestartGameManager : MonoBehaviour {
 
+	public Button yourButton;
+
+	public void ClickTest ()
+	{
+		Debug.Log ("Yes, am here");
+	}
+
+	void Start () {
+		Button btn = yourButton.GetComponent<Button>();
+		btn.onClick.AddListener(RestartLevel);
+	}
 
 	public void RestartLevel ()
 	{
+		Debug.Log ("Yes, am here");
 		// Reload the level that is currently loaded.
-		Application.LoadLevel(Application.loadedLevelName);
+		SceneManager.LoadScene(0);
 	}
 }
