b20b5836cc6e8cd8c0214e3c1d36442488413119
AseelNaji <gusnajias@student.gu.se>
2016-12-10 13:29:32 +0100

    Fixed line renderer with @Ninosauleo
    Nino’s computer is damaged so we will work together here.

---
+++ Game/Run Doris Run!/Assets/Scripts/Managers/restartGame.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class RestartGame : MonoBehaviour {
+	
+
+	public void RestartLevel ()
+	{
+		// Reload the level that is currently loaded.
+		SceneManager.LoadScene (0);
+	}
+}
---
+++ Game/Run Doris Run!/Assets/_CompletedAssets/Scripts/Managers/RestartGameManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class RestartGameManager : MonoBehaviour {
+
+
+	public void RestartLevel ()
+	{
+		// Reload the level that is currently loaded.
+		Application.LoadLevel(Application.loadedLevelName);
+	}
+}
